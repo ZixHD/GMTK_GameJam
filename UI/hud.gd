67a4a919ec5d69b245b85fe9e2ecc14b3b75e3eb
ZixHD <havos25@gmail.com>
2025-07-31 18:04:04 +0200

    Kuca dodata i provera za hud

--- UI/hud.gd
+++ UI/hud.gd
@@ -4,3 +4,12 @@ extends CanvasLayer
 
 func _ready() -> void:
 	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		print("opvde")
+		var tex_rect = TextureRect.new()
+		var texture = load("res://icon.svg")
+		tex_rect.texture = texture
+		tex_rect.stretch_mode = TextureRect.STRETCH_SCALE
+		v_box_container.add_child(tex_rect)
