80dda32520ef46adaf251410b44b09c8cdbf3f77
cinga333 <cernjevnikola@gmail.com>
2025-07-31 22:32:38 +0200

    ribarska kuca transparent

--- Game/Singleton/GameManager/game_manager.gd
+++ Game/Singleton/GameManager/game_manager.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 const REAL_SECONDS_PER_DAY = 120
-const START_TIME_HOUR = 12  # 6= 06:00 AM, 12= 12:00 etc...
+const START_TIME_HOUR = 6  # 6= 06:00 AM, 12= 12:00 etc...
 const START_TIME_MINUTE = 0 
 
 #Game logic and consts
