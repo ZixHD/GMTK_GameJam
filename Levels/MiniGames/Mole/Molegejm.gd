1308db34fa5ea3b0b65ca076efe2fd779b0675a8
ZixHD <havos25@gmail.com>
2025-08-01 23:26:34 +0200

    Mole minigame gotov

--- Levels/MiniGames/Mole/Molegejm.gd
+++ Levels/MiniGames/Mole/Molegejm.gd
@@ -1,14 +1,20 @@
 extends Node2D
-@onready var krtina_1: Sprite2D = $Krtina1
-@onready var krtina_2: Sprite2D = $Krtina2
-@onready var krtina_3: Sprite2D = $Krtina3
-@onready var krtina_4: Sprite2D = $Krtina4
+
+@export var mole_visible_time := 2.0
+@export var mole_spawn_min := 1.0
+@export var mole_spawn_max := 2.5
+@export var victory_pause_time := 6.0
+
+@onready var krtina_1: Sprite2D = $Krtina1/Krtina1
+@onready var krtina_2: Sprite2D = $Krtina2/Krtina2
+@onready var krtina_3: Sprite2D = $Krtina3/Krtina3
+@onready var krtina_4: Sprite2D = $Krtina4/Krtina4
 @onready var score_label: Label = $score_label
 @onready var victory: Label = $victory
 
 @onready var moles = [$Krtina1, $Krtina2, $Krtina3, $Krtina4]
 var score := 0
-
+var last_mole_index := -1   # pamti poslednju krticu koja se pojavila
 
 func _ready():
 	randomize()
@@ -20,30 +26,42 @@ func start_mole_cycle():
 func mole_timer():
 	for mole in moles:
 		mole.visible = false
-		mole.get_node("Area2D/CollisionShape2D").disabled = true 
+		mole.get_node("CollisionShape2D").disabled = true 
 
-	var random_mole = moles[randi() % moles.size()]
+	var new_index = pick_new_mole_index()
+	var random_mole = moles[new_index]
+	last_mole_index = new_index
+
 	random_mole.visible = true
-	random_mole.get_node("Area2D/CollisionShape2D").disabled = false
-# stoji 3 sekunde
-	await get_tree().create_timer(3.0).timeout #treba da se promeni i u ceke
+	random_mole.get_node("CollisionShape2D").disabled = false
+
+	await get_tree().create_timer(mole_visible_time).timeout
 	random_mole.visible = false
-	random_mole.get_node("Area2D/CollisionShape2D").disabled = false
-	
-	# mole se spawnuje izmedju 1 - 2.5
-	var random_vreme = randf_range(1.0, 2.5)
+	random_mole.get_node("CollisionShape2D").disabled = false
+
+	var random_vreme = randf_range(mole_spawn_min, mole_spawn_max)
 	await get_tree().create_timer(random_vreme).timeout
 	print("Sledeća krtica izlazi za: ", random_vreme, " sekundi") 
 	mole_timer()
+
+func pick_new_mole_index() -> int:
+	var index = randi() % moles.size()
+	while index == last_mole_index:
+		index = randi() % moles.size()
+	return index
 
 func add_score():
 	score += 1
 	if score_label:
 		score_label.text = str(score)
 		print("Score: ", score)
-		
+		#Display Numbers, and sound
 		if score >= 5: 
+			krtina_1.visible = false
+			krtina_2.visible = false
+			krtina_3.visible = false
+			krtina_4.visible = false	
 			victory.visible = true
 			get_tree().paused = true
-			await get_tree().create_timer(6.0).timeout
+			await get_tree().create_timer(victory_pause_time).timeout
 			print("pobeda")
--- Levels/MiniGames/Mole/cekic.gd
+++ Levels/MiniGames/Mole/cekic.gd
@@ -1,9 +1,23 @@
-extends Node2D   
+extends Node2D
 
-@export var speed := 400    
+@export var speed := 400
+@export var swing_duration := 0.2
+@export var cooldown_duration := 1.0
+@export var respawn_delay := 3.0
+@onready var dust_particles_jump_left: GPUParticles2D = $Cekic/DustParticlesJumpLeft
+@onready var dust_particles_jump_right: GPUParticles2D = $Cekic/DustParticlesJumpRight
+
 @onready var area: Area2D = $Cekic/Area2D
 
-var is_hitting := false
+var can_hit := true
+
+
+func _spawn_jump_dust() -> void:
+	dust_particles_jump_left.restart()
+	dust_particles_jump_left.emitting = true
+	dust_particles_jump_right.restart()
+	dust_particles_jump_right.emitting = true;
+	
 
 func _physics_process(delta):
 	var move_dir = Vector2.ZERO
@@ -20,51 +34,37 @@ func _physics_process(delta):
 	position += move_dir.normalized() * speed * delta
 
 func _input(event):
-	if event.is_action_pressed("Hit") and not is_hitting:
+	if event.is_action_pressed("Hit") and can_hit:
 		play_hit()
 
 func play_hit():
-	is_hitting = true
-	area.monitoring = true   
+	can_hit = false
 
+	
 	var tween = create_tween()
 	tween.tween_property(self, "rotation_degrees", 50, 0.1)
 	tween.tween_property(self, "rotation_degrees", 0, 0.1).set_delay(0.1)
-
-	print("Overlapping areas:", area.get_overlapping_areas())
-
-	for mole in area.get_overlapping_areas():
-		print("boze")
-		if mole.visible:
-			mole.visible = false
-			print("HIT:", mole.name)
-			get_parent().add_score()
-
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.1).timeout
+	area.monitoring = true
+	_spawn_jump_dust()
+	#particles
+	
+	await get_tree().create_timer(swing_duration).timeout
 	area.monitoring = false
-	await get_tree().create_timer(1.0).timeout
-	is_hitting = false
 
+	await get_tree().create_timer(cooldown_duration).timeout
+	can_hit = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("radi")
-	pass # Replace with function body.
-
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if is_hitting:
+	if not can_hit:
 		if area.visible:
 			area.visible = false
 			get_parent().add_score()
-
-		#collider after short delay
-		await get_tree().create_timer(0.2).timeout
+			DamageNumbers.display_number("+1", area.get_node("Marker2D").global_position)
+		await get_tree().create_timer(swing_duration).timeout
 		area.monitoring = false
-		#nakon 3 sec se spawnuje collision
-		await get_tree().create_timer(3.0).timeout
-		area.visible = true
-		#tek nakon 1.5s trenutno moze opet da udari,
-		await get_tree().create_timer(2.0).timeout
-		is_hitting = false
+		await get_tree().create_timer(respawn_delay).timeout
 		
-	
